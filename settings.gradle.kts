bbc609d62815119710fd69d582bae10b98a4eb49
navinkumar1288 <89594000+navinkumar1288@users.noreply.github.com>
2025-11-09 20:29:23 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,24 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    // Optional: resolutionStrategy if you need to force plugin versions
+    plugins {
+        // declare the kotlin gradle plugin here so it's resolved consistently
+        id("org.jetbrains.kotlin.jvm") version "1.9.20"
+        id("com.android.application") version "8.1.2"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "IrrigCtrl-Android"
 include(":app")
