5c5e059280d9fe6a0c058fe5de542f32b6332772
navinkumar1288 <89594000+navinkumar1288@users.noreply.github.com>
2025-11-09 20:17:20 +0530

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("android") version "1.9.20" apply false
+    id("com.android.application") version "8.1.2" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
